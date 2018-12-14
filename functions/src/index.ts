4e52a34e1b61c9ac8d9eff61e00b9cc62d2a8143
MitaniWataru <attzlz.t3tzaz@softbank.ne.jp>
2018-12-14 21:18:24 +0900

    大阪と福岡追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,3 +6,12 @@ import * as functions from 'firebase-functions';
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("こんにちはファイアベース！！");
  });
+
+ export const Hukuoka = functions.https.onRequest((request, response) => {
+     response.send("nasitoーto");
+ });
+
+ export const Osaka = functions.https.onRequest((request, response) =>{
+     response.send("なにしてんねん");
+ });
+
